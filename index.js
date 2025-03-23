e98f20824f3dacfee353f3fa0619f8b0c4a3b5ce
blackkat02 <138970349+blackkat02@users.noreply.github.com>
2025-03-23 19:35:27 +0000

    Deploying to gh-pages from @ blackkat02/goit-advancedjs-hw-03@ce3294e42ab09ca4fb897470d88b86a4f6427e1f 🚀

--- index.js
+++ index.js
@@ -1,24 +1,24 @@
-import"./assets/styles-EQ2dEGpi.js";import{i as g,S as y}from"./assets/vendor-CoygeA8O.js";const d=document.querySelector(".gallery"),v=document.querySelector("form"),b="49334918-ea87b03a35c8c75cd9fd8a30d";let l;const L=()=>{g.show({title:"Invalid Date",message:"Please choose a date in the future",backgroundColor:"#ff6666",position:"topRight"})};function c(a=!1){const t=document.getElementById("loader");t.style.display=a?"block":"none"}function w(a){a.preventDefault(),c(!0);const t=a.target.querySelector("input[name='query']");if(!t||t.value.trim()===""){console.log("Поле пошуку порожнє!"),c(!1);return}const n=t.value.trim();console.log("Шукаємо:",n),$(n).catch(e=>console.error("Помилка при отриманні фото:",e)).finally(()=>{c(!1),t.value=""})}v.addEventListener("submit",w);const S={key:b,image_type:"photo",orientation:"horizontal",safesearch:"true"};function $(a){const n=`https://pixabay.com/api/?${new URLSearchParams({...S,q:a})}`;return fetch(n).then(e=>{if(!e.ok)throw new Error(`HTTP error! Status: ${e.status}`);return e.json()}).then(e=>{if(e.hits.length===0){console.log("Нічого не знайдено."),L(),d.innerHTML="";return}e.hits.forEach(s=>{const{webformatURL:i,largeImageURL:r,tags:o,likes:h,views:u,comments:f,downloads:m}=s;p({webformatURL:i,largeImageURL:r,tags:o,likes:h,views:u,comments:f,downloads:m}),E(e.hits)})}).catch(e=>console.error("Помилка при отриманні даних:",e))}function p({webformatURL:a,largeImageURL:t,tags:n,likes:e,views:s,comments:i,downloads:r}){const o=document.createElement("div");o.classList.add("photo-card"),o.innerHTML=`
-    <a href="${t}" class="gallery-link">
-      <img src="${a}" alt="${n}" loading="lazy" />
-    </a>
-    <div class="info">
-      <div class="info-item">
-        <span><b>Likes</b></span>
-        <span>${e}</span>
+import{i as g,S as y}from"./assets/vendor-B2mb6eXk.js";(function(){const t=document.createElement("link").relList;if(t&&t.supports&&t.supports("modulepreload"))return;for(const e of document.querySelectorAll('link[rel="modulepreload"]'))o(e);new MutationObserver(e=>{for(const r of e)if(r.type==="childList")for(const a of r.addedNodes)a.tagName==="LINK"&&a.rel==="modulepreload"&&o(a)}).observe(document,{childList:!0,subtree:!0});function s(e){const r={};return e.integrity&&(r.integrity=e.integrity),e.referrerPolicy&&(r.referrerPolicy=e.referrerPolicy),e.crossOrigin==="use-credentials"?r.credentials="include":e.crossOrigin==="anonymous"?r.credentials="omit":r.credentials="same-origin",r}function o(e){if(e.ep)return;e.ep=!0;const r=s(e);fetch(e.href,r)}})();const d=document.querySelector(".gallery");let l;const v=()=>{g.show({title:"Invalid Request",message:"Sorry, there are no images matching your search query. Please try again!",backgroundColor:"#ff6666",position:"topRight"})};function u({webformatURL:n,largeImageURL:t,tags:s,likes:o,views:e,comments:r,downloads:a}){const i=document.createElement("div");i.classList.add("photo-card"),i.innerHTML=`
+      <a href="${t}" class="gallery-link">
+        <img src="${n}" alt="${s}" loading="lazy" />
+      </a>
+      <div class="info">
+        <div class="info-item">
+          <span><b>Likes</b></span>
+          <span>${o}</span>
+        </div>
+        <div class="info-item">
+          <span><b>Views</b></span>
+          <span>${e}</span>
+        </div>
+        <div class="info-item">
+          <span><b>Comments</b></span>
+          <span>${r}</span>
+        </div>
+        <div class="info-item">
+          <span><b>Downloads</b></span>
+          <span>${a}</span>
+        </div>
       </div>
-      <div class="info-item">
-        <span><b>Views</b></span>
-        <span>${s}</span>
-      </div>
-      <div class="info-item">
-        <span><b>Comments</b></span>
-        <span>${i}</span>
-      </div>
-      <div class="info-item">
-        <span><b>Downloads</b></span>
-        <span>${r}</span>
-      </div>
-    </div>
-  `,d.appendChild(o)}function E(a){d.innerHTML="",a.forEach(t=>{p(t)}),l?l.refresh():l=new y(".gallery a",{captionsData:"alt",captionDelay:250})}
+    `,d.appendChild(i)}function b(n){d.innerHTML="",n.forEach(t=>{u(t)}),l?l.refresh():l=new y(".gallery a",{captionsData:"alt",captionDelay:250})}const L="49334918-ea87b03a35c8c75cd9fd8a30d",w={key:L,image_type:"photo",orientation:"horizontal",safesearch:"true"};function c(n=!1){const t=document.getElementById("loader");t.style.display=n?"block":"none"}function S(n){console.log("banana-start"),n.preventDefault(),c(!0);const t=n.target.querySelector("input[name='query']");if(!t||t.value.trim()===""){console.log("Поле пошуку порожнє!"),c(!1),t.value="";return}const s=t.value.trim();console.log("Шукаємо:",s),E(s).catch(o=>console.error("Помилка при отриманні фото:",o)).finally(()=>{c(!1),t.value=""})}function E(n){const s=`https://pixabay.com/api/?${new URLSearchParams({...w,q:n})}`;return fetch(s).then(o=>{if(!o.ok)throw new Error(`HTTP error! Status: ${o.status}`);return o.json()}).then(o=>{if(o.hits.length===0){console.log("Нічого не знайдено."),d.innerHTML="",v();return}o.hits.forEach(e=>{const{webformatURL:r,largeImageURL:a,tags:i,likes:f,views:p,comments:m,downloads:h}=e;u({webformatURL:r,largeImageURL:a,tags:i,likes:f,views:p,comments:m,downloads:h}),b(o.hits)})}).catch(o=>console.error("Помилка при отриманні даних:",o))}const P=document.querySelector("form");P.addEventListener("submit",S);
 //# sourceMappingURL=index.js.map
